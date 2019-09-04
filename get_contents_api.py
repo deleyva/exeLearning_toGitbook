fe0ccf079ff1a30553c35904f49fc6db94627b43
deleyva <jesuslopezdeleyva@gmail.com>
2019-09-04 14:20:03 +0200

    falta convertir en markdown el contenido

--- get_contents_api.py
+++ get_contents_api.py
@@ -3,19 +3,39 @@ from bs4 import BeautifulSoup
 import re
 from slugify import slugify
 
-def get_items(url, pattern):
+def scrapying_function(url):
+    r = requests.get(url)
+    soup_object = BeautifulSoup(r.text, 'html.parser')
+    return soup_object
+
+def get_course_title_folder(url):
+    soup = scrapying_function(url)
+    return slugify(soup.title.text[7:])
+
+def get_items(url, pattern=''):
     '''
-    Cojo los elementos que quiere scrapear de una web de curso de Moodle
+    Cojo los elementos que quiero scrapear de una web de curso de Moodle
     Parametros: url, expresión regular a encontrar
     Returns: lista de urls
     '''
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = scrapying_function(url)
     anclas = soup.find_all('a')
     sources = [ancla.get('href') for ancla in anclas if re.search(pattern, ancla.get('href')) is not None]
     return sources
 
-def get_moodle_page(url):
+def get_images(soup_object):
+    '''
+    Descarga todas las imágenes de una página a la carpeta ./img/
+    '''
+    content_soup = soup_object.find('div', {'role':'main'})
+    images_urls = [img['src'] for img in content_soup.find_all('img')]
+    urls_and_files = [(item,item.split('/')[-1]) for item in images_urls]
+    for item in urls_and_files:
+        url_r = requests.get(item[0])
+        file = item[1]
+        open('./img/{}'.format(file), 'wb').write(url_r.content)
+
+def get_moodle_page(url, counter):
     '''
     Devuelve un objeto con los siguientes atributos:
     * tipo: si es página o libro
@@ -23,17 +43,18 @@ def get_moodle_page(url):
     * archive: nombre del archivo más .md
     * html: todo el html del contenido de la página
     
-    Parametros: url
+    Parametros: url, counter
 
     Returns: un objeto con los contenidos de la página
     '''
     page = {}
+    page['id'] = counter
     page['tipo'] = 'page' if 'mod/page' in url else 'book'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = scrapying_function(url)
     page['title'] = soup.title.text
     page['archive'] = slugify(soup.title.text) + '.md'
     page['html'] = str(soup.find('div', {'role':'main'}))
+    get_images(soup)
     return page
 
 def get_moodle_book_chapters(url):
@@ -42,15 +63,19 @@ def get_moodle_book_chapters(url):
     Parametros: url
     Returns: una lista urls.
     '''
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = scrapying_function(url)
     toc = soup.find('div', {'class':'book_toc_numbered'})
     anclas = toc.find_all('a')
     sources = [url + '&' + str(ancla.get('href')).split('&')[-1] for ancla in anclas]
     return sources
 
-def summary_generation(jsonfile):
+def summary_generation(list_of_dictionaries):
     '''
     Genera un archivo SUMMARY.md para gitbook
     '''
-    pass
+    dictionaries_sorted = sorted(list_of_dictionaries, key=lambda i: i['id'])
+    list_of_pages = [[item['title'], item['archive']] for item in dictionaries_sorted]
+    for item in list_of_pages:
+        with open('SUMMARY.md', 'a') as summary:
+            summary.write("* [{}]({})".format(item[0],item[1]))
+
--- pages-moodle-to-markdown.py
+++ pages-moodle-to-markdown.py
@@ -1,26 +1,50 @@
 from get_contents_api import *
 import json
 import time
+import os
+
+PATH_FOR_BOOKS = str(os.getcwd() + '/books_pushed')
+PATH_FOR_ARCHIVES_CREATED = str(os.getcwd() + '/books_to_push')
 
 main_url = 'https://moodle.catedu.es/course/view.php?id=7' # input('Introduce la url: ')
 pattern = r'(mod/book|mod/page)' # input('Introduce la expresión regular: ')
+# repo = input('Introduce el repo de github: ')
 pages = []
 
+# Creo las carpetas y copio los archivos básicos
+folder = get_course_title_folder(main_url)
+path = PATH_FOR_ARCHIVES_CREATED + '/' + folder
+os.mkdir(path)
+os.chdir(path)
+os.mkdir(path + '/img')
+os.system('gitbook init')
+os.system('cp ../../book.json .')
+os.system('cp ../../.gitignore .')
+os.system('cp ../../FOOTER.md .')
+os.system('cp ../../crditos-buenos.md .')
+
 sources = get_items(main_url, pattern)
+
+counter = 0
 
 for source in sources:
     time.sleep(2)
     if 'mod/page' in source:
-        page = get_moodle_page(source)
+        page = get_moodle_page(source, counter)
         pages.append(page)
+        counter += 1
     else:
-        page = get_moodle_page(source)
+        page = get_moodle_page(source, counter)
         pages.append(page)
+        counter += 1
         sub_pages = get_moodle_book_chapters(source)
         for item in sub_pages:
             time.sleep(2)
-            page = get_moodle_page(item)
+            page = get_moodle_page(item, counter)
             pages.append(page)
+            counter += 1
 
-with open('outputfile.json', 'w') as fout:
-    json.dump(pages, fout)
+with open('outputfile.json', 'w', encoding='utf8') as fout:
+    json.dump(pages, fout, ensure_ascii=False)
+
+summary_generation(pages)
